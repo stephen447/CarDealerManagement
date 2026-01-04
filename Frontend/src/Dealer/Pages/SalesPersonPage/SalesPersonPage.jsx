74161a1758243d00e1196e2ad38abd79f799a4b4
stephen447 <byrnes67@tcd.ie>
2026-01-04 19:55:05 +0000

    error handling for salesperson page

--- Frontend/src/Dealer/Pages/SalesPersonPage/SalesPersonPage.jsx
+++ Frontend/src/Dealer/Pages/SalesPersonPage/SalesPersonPage.jsx
@@ -6,113 +6,146 @@ import styles from "./SalesPersonPage.module.css";
 import generalStyles from "../../../General/Other/GeneralStyles.module.css";
 import TextInput from "../../../General/Component/TextInput/TextInput";
 import axiosInstance from "../../../General/Other/AxiosInstance";
+import Loader from "../../../General/Component/Loader/Loader";
+import Warning from "../../../General/Component/Warning/Warning";
 
 function SalesPersonPage() {
   const { id } = useParams();
   const [salesPerson, setSalesPerson] = useState(null);
   const [tempSalesPerson, setTempSalesPerson] = useState(null);
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [warning, setWarning] = useState("");
+  const [updateLoading, setUpdateLoading] = useState(false);
+  const [updateWarning, setUpdateWarning] = useState("");
 
   // fetch sales person data on load
   useEffect(() => {
-    // ToDo Fetch from the db
-    axiosInstance.get(`/api/v1/user/${id}`).then((response) => {
-      console.log(response.data);
-      setSalesPerson({
-        firstName: response.data.firstName,
-        lastName: response.data.lastName,
-        email: response.data.email,
-      });
-      setTempSalesPerson({
-        firstName: response.data.firstName,
-        lastName: response.data.lastName,
-        email: response.data.email,
-      });
-    });
-  }, [id]);
-
-  const handleSave = () => {
-    // ToDo Save to the db
-    axiosInstance
-      .put(`/api/v1/user/${id}`, tempSalesPerson)
-      .then((response) => {
+    async function apiCall() {
+      try {
+        setLoading(true);
+        const response = await axiosInstance.get(`/api/v1/user/${id}`);
         console.log(response.data);
         setSalesPerson({
           firstName: response.data.firstName,
           lastName: response.data.lastName,
           email: response.data.email,
         });
-        setEditMode(false);
+        setTempSalesPerson({
+          firstName: response.data.firstName,
+          lastName: response.data.lastName,
+          email: response.data.email,
+        });
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setWarning("Error fetching salesperson");
+        setLoading(false);
+      }
+    }
+    apiCall();
+  }, [id]);
+
+  const handleSave = async () => {
+    try {
+      setUpdateLoading(true);
+      const response = await axiosInstance.put(
+        `/api/v1/user/${id}`,
+        tempSalesPerson
+      );
+      console.log(response.data);
+      setSalesPerson({
+        firstName: response.data.firstName,
+        lastName: response.data.lastName,
+        email: response.data.email,
       });
-    setSalesPerson(tempSalesPerson);
-    setEditMode(false);
+      setUpdateLoading(false);
+      setEditMode(false);
+    } catch (error) {
+      console.error(error);
+      setUpdateWarning("Error updating salesperson");
+      setUpdateLoading(false);
+    }
   };
 
   const handleCancel = () => {
     setEditMode(false);
     setTempSalesPerson(salesPerson);
+    setUpdateWarning("");
   };
 
   return (
     <div>
       <Header />
       <main className={generalStyles["main-container"]}>
-        {salesPerson !== null ? (
-          editMode ? (
-            <div className={generalStyles["content-container"]}>
-              <TextInput
-                label="First Name"
-                name="firstName"
-                type="text"
-                formData={tempSalesPerson}
-                setFormData={setTempSalesPerson}
-              />
-              <TextInput
-                label="Last Name"
-                name="lastName"
-                type="text"
-                formData={tempSalesPerson}
-                setFormData={setTempSalesPerson}
-              />
-              <TextInput
-                label="Email"
-                name="email"
-                type="email"
-                formData={tempSalesPerson}
-                setFormData={setTempSalesPerson}
-              />
-              <div className={styles["button-container"]}>
-                <button
-                  onClick={() => handleCancel()}
-                  className={generalStyles["button-primary"]}
-                >
-                  Cancel
-                </button>
-                <button
-                  onClick={() => handleSave()}
-                  className={generalStyles["button-primary"]}
-                >
-                  Save
-                </button>
-              </div>
-            </div>
-          ) : (
-            <div className={generalStyles["content-container"]}>
-              <p>First Name: {salesPerson.firstName}</p>
-              <p>Last Name: {salesPerson.lastName}</p>
-              <p>Email: {salesPerson.email}</p>
-              <div className={styles["button-container"]}>
-                <button
-                  onClick={() => setEditMode(true)}
-                  className={generalStyles["button-primary"]}
-                >
-                  Edit
-                </button>
-              </div>
-            </div>
-          )
+        {loading ? (
+          <div className={generalStyles["content-container"]}>
+            <Loader />
+          </div>
+        ) : warning ? (
+          <div className={generalStyles["content-container"]}>
+            <Warning message={warning} />
+          </div>
+        ) : editMode ? (
+          <div className={generalStyles["content-container"]}>
+            {updateLoading ? (
+              <Loader />
+            ) : (
+              <>
+                <TextInput
+                  label="First Name"
+                  name="firstName"
+                  type="text"
+                  formData={tempSalesPerson}
+                  setFormData={setTempSalesPerson}
+                />
+                <TextInput
+                  label="Last Name"
+                  name="lastName"
+                  type="text"
+                  formData={tempSalesPerson}
+                  setFormData={setTempSalesPerson}
+                />
+                <TextInput
+                  label="Email"
+                  name="email"
+                  type="email"
+                  formData={tempSalesPerson}
+                  setFormData={setTempSalesPerson}
+                />
+
+                <div className={styles["button-container"]}>
+                  <button
+                    onClick={handleCancel}
+                    className={generalStyles["button-primary"]}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={handleSave}
+                    className={generalStyles["button-primary"]}
+                  >
+                    Save
+                  </button>
+                </div>
+                {updateWarning && <Warning message={updateWarning} />}
+              </>
+            )}
+          </div>
         ) : (
-          <h1 className={styles["loading"]}>Loading...</h1>
+          <div className={generalStyles["content-container"]}>
+            <p>First Name: {salesPerson.firstName}</p>
+            <p>Last Name: {salesPerson.lastName}</p>
+            <p>Email: {salesPerson.email}</p>
+            <div className={styles["button-container"]}>
+              <button
+                onClick={() => setEditMode(true)}
+                className={generalStyles["button-primary"]}
+              >
+                Edit
+              </button>
+            </div>
+          </div>
         )}
       </main>
     </div>
