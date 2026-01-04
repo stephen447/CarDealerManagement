45bc4819e792fc06033454380445dc380ab4873b
stephen447 <byrnes67@tcd.ie>
2026-01-04 17:26:59 +0000

    Added error handling to create salesperson

--- Frontend/src/Dealer/Pages/CreateSalesPersonPage/CreateSalesPersonPage.jsx
+++ Frontend/src/Dealer/Pages/CreateSalesPersonPage/CreateSalesPersonPage.jsx
@@ -5,6 +5,8 @@ import SidebarNav from "../../../General/SideBarNav/SideBarNav";
 import DealerNavLinks from "../../../General/Other/DealerNavLinks";
 import generalStyles from "../../../General/Other/GeneralStyles.module.css";
 import axiosInstance from "../../../General/Other/AxiosInstance";
+import Loader from "../../../General/Component/Loader/Loader";
+import Warning from "../../../General/Component/Warning/Warning";
 
 function CreateSalesPersonPage() {
   const [formData, setFormData] = useState({
@@ -15,27 +17,38 @@ function CreateSalesPersonPage() {
     role: "SALESPERSON",
     dealerId: "1234",
   });
+
+  const [warning, setWarning] = useState("");
+  const [loading, setLoading] = useState(false);
   const navItems = DealerNavLinks.salesPeople;
 
-  const handleSubmit = (e) => {
+  /**
+   * Function for sending a post request to the API to create a new salesperson in DB
+   * @param {Event} e - The event object
+   */
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    setWarning("");
+    setLoading(true);
 
-    // Replace with API call later
-    console.log("Creating salesperson:", formData);
-
-    axiosInstance.post("/api/v1/user", formData).then((response) => {
-      console.log(response.data);
-    });
-
-    // Optional: reset form
-    setFormData({
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-      role: "SALESPERSON",
-      dealerId: "1234",
-    });
+    try {
+      const response = await axiosInstance.post("/api/v1/user", formData);
+      // Optional: reset form
+      setFormData({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        role: "SALESPERSON",
+        dealerId: "1234",
+      });
+      setLoading(false);
+      // TODO:Could maybe redirect to the salesperson page
+    } catch (error) {
+      console.error("Error creating salesperson:", error);
+      setLoading(false);
+      setWarning("Error creating salesperson");
+    }
   };
 
   return (
@@ -74,10 +87,14 @@ function CreateSalesPersonPage() {
               formData={formData}
               setFormData={setFormData}
             />
-
-            <button type="submit" className={generalStyles["button-primary"]}>
-              Create Sales Person
-            </button>
+            {loading ? (
+              <Loader />
+            ) : (
+              <button type="submit" className={generalStyles["button-primary"]}>
+                Create Sales Person
+              </button>
+            )}
+            {warning && <Warning message={warning} />}
           </form>
         </div>
       </main>
